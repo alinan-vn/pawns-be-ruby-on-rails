f5b5f8093a4d03c162e56c4d3c1d9d76a1c4d4ac
alinan-vn <jandrolinan@gmail.com>
2020-05-25 20:24:29 -0500

    dropped date column from articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_025956) do
+ActiveRecord::Schema.define(version: 2020_05_26_012255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2020_05_23_025956) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.string "date"
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
